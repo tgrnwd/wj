15d7febaf7efbf96e472c38d3019fea6feb01888
tgrnwd <tgrnwd@gmail.com>
2019-05-09 12:00:32 -0500

    update static file path

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client')));
 
 // API
 app.get('/api/test', (req, res) => {
@@ -18,7 +18,7 @@ app.get('/api/test', (req, res) => {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  res.sendFile(path.join(__dirname + '/client/index.html'));
 });
 
 const port = process.env.PORT || 5000;
