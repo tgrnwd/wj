0c81e09c5802664399d4312dfe3f3845073708b8
tgrnwd <tgrnwd@gmail.com>
2019-05-09 21:46:33 -0500

    update now

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client')));
 
 // API
 app.get('/api/test', (req, res) => {
   const count = 5
   let random = Array.from(Array(count).keys()).map(x => Math.random() * Math.floor(100))
-  
   res.json(random);
   console.log(`Sent JSON`);
 });
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/index.html'));
-});
 
-const port = process.env.PORT || 5000;
+
+const port = process.env.PORT || 3000;
 app.listen(port);
-
 console.log(`App listening on ${port}`);
